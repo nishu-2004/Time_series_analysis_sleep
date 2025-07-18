619a574c3165cf966fc6c298a49012659bc7d1ab
Nishanth P Kashyap <113301662+nishu-2004@users.noreply.github.com>
2025-07-18 14:16:07 +0530

    Update Activity_cleaning_pipeline.py

--- cleaning_pipelines/Activity_cleaning_pipeline.py
+++ cleaning_pipelines/Activity_cleaning_pipeline.py
@@ -3,7 +3,7 @@ import mysql.connector
 conn = mysql.connector.connect(
     host='localhost',
     user='root',
-    password='Kashyap@123!',
+    password='',
     database='workout_sleep'
 )
 cursor = conn.cursor()
